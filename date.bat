0aa25f0d0cd88e52567260b7f2c4900d2c5ee204
Andrew Fehr <andrewfehr@outlook.com>
2020-01-24 13:13:29 -0500

    Update date.bat

--- date.bat
+++ date.bat
@@ -1,1 +1,1 @@
-C:\ProgramData\chocolatey\lib\unxUtils\tools\unxUtils\usr\local\wbin\date.exe +%y.%-m.%d.0>stuff.txt
+C:\ProgramData\chocolatey\lib\unxUtils\tools\unxUtils\usr\local\wbin\date.exe +%%y.%%-m.%%d.0>stuff.txt
